2c5a1dbdf43a1710813d632fff4088a354ce4678
Salahdine Ouhsain <souhsain@e2r4p13.1337.ma>
2026-01-23 13:16:17 +0100

    put

--- module01/ex5/ft_plant_types.py
+++ module01/ex5/ft_plant_types.py
@@ -1,49 +1,50 @@
-class Planst:
-    def __init__(self, name: str, height: int, age: int):
+class Plant:
+    def __init__(self, name: str, height: int, age: int) -> None:
         self.name = name
         self.age = age
         self.height = height
 
 
-class Tree(Planst):
-    def __init__(self, name: str, height: int, age: int, diameter: int):
+class Tree(Plant):
+    def __init__(self, name: str, height: int, age: int,
+                 diameter: int) -> None:
         super().__init__(name, height, age)
         self.trunk_diameter = diameter
-        print(f"{name} (Tree): {height}cm, {age} days, {diameter} diameter")
+        print(f"{name} (Tree): {height}cm, {age} days, {diameter}cm diameter")
 
-    def produce_shade(self):
-        shade_area = 0.1 * self.height + 0.56 * self.trunk_diameter
+    def produce_shade(self) -> None:
+        shade_area = int(0.1 * self.height + 0.56 * self.trunk_diameter)
         print(f"{self.name} provides {shade_area} square meters of shade")
 
 
-class Flower(Planst):
-    def __init__(self, name: str, height: int, age: int, color: str):
+class Flower(Plant):
+    def __init__(self, name: str, height: int, age: int, color: str) -> None:
         super().__init__(name, height, age)
         self.color = color
         print(f"{name} (Flower): {height}cm, {age} days, {color} color")
 
-    def bloom(self):
+    def bloom(self) -> None:
         print(f"{self.name} is blooming beautifully!")
 
 
-class Vegetable(Planst):
-    def __init__(self, name: str, height: int, age: int, season: str):
+class Vegetable(Plant):
+    def __init__(self, name: str, height: int, age: int, season: str) -> None:
         super().__init__(name, height, age)
         self.season = season
         print(f"{name} (Vegetable): {height}cm, {age} days, {season} harvest")
 
-    def show_nutrition(self):
+    def show_nutrition(self) -> None:
         value: str
         if self.season == "summer":
-            value = "Vitamin C"
+            value = "vitamin C"
         elif self.season == "fall":
-            value = "Vitamin A"
+            value = "vitamin A"
         elif self.season == "winter":
-            value = "Vitamin D"
+            value = "vitamin D"
         elif self.season == "spring":
-            value = "Vitamin E"
+            value = "vitamin E"
         else:
-            value = "Vitamin C"
+            value = "vitamin C"
 
         print(f"{self.name} is rich in {value}")
 
@@ -52,7 +53,9 @@ if __name__ == "__main__":
     print("=== Garden Plant Types ===\n")
     rose = Flower("Rose", 25, 30, "red")
     rose.bloom()
+    print(end="\n")
     oak = Tree("Oak", 500, 1825, 50)
     oak.produce_shade()
+    print(end="\n")
     tomato = Vegetable("Tomato", 80, 90, "summer")
     tomato.show_nutrition()
