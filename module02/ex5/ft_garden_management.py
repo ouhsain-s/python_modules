5487deed3d00f1c50d4a68f7347f0c790eb610b7
Salahdine Ouhsain <souhsain@e1r8p2.1337.ma>
2026-01-29 15:26:23 +0100

    add

--- module02/ex5/ft_garden_management.py
+++ module02/ex5/ft_garden_management.py
@@ -1,12 +1,18 @@
 
-class PlantError(Exception):
+class GardenError(Exception):
     def __init__(self, message: str):
         super().__init__(message)
 
 
-class WaterError(Exception):
+class PlantError(GardenError):
     def __init__(self, message: str):
         super().__init__(message)
+
+
+class WaterError(GardenError):
+    def __init__(self, message: str):
+        super().__init__(message)
+
 
 class Plant:
     def __init__(self, name: str, water: int, sun: int):
@@ -39,4 +45,21 @@ class GardenManager:
                     print("")
         except WaterError as e:
             print(e)
-    
+        finally:
+            print("")
+
+    def check_plant_health(plant_name, water_level):
+        if plant_name == "" or plant_name is None:
+            raise PlantError("")
+        if water_level < 1:
+            raise WaterError(f"")
+        if water_level > 10:
+            raise WaterError(f"")
+
+    def checking_health(cls, garden: Garden):
+        try:
+            for plant in garden.plants:
+                cls.check_plant_health(plant.name, plant.wather, plant.sun)
+            print("")
+        except GardenError as e:
+            print(e)
