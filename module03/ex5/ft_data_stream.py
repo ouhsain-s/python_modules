47f375282480ef53fdc8e8d06ee49a40f2266eb4
Salahdine Ouhsain <souhsain@e2r6p14.1337.ma>
2026-02-04 13:13:39 +0100

    add

--- module03/ex5/ft_data_stream.py
+++ module03/ex5/ft_data_stream.py
@@ -26,10 +26,48 @@ def compute_time():
     print("Processing time: 0.045 seconds")
 
 
+def fibonacci_sequence(count) -> Generator:
+    n1 = 0
+    n2 = 1
+    tmp = 0
+    for i in range(count):
+        yield n1
+        tmp = n1
+        n1 = n2
+        n2 += tmp
+
+
+def prime_numbers():
+    yield 2
+    for i in range(3, 100000):
+        is_prime = True
+        for j in range(2, i):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            yield i
+
+
 def generator_demonstration():
-    print("")
-    print("")
-    print("")
+    count = 10
+    t = count
+    print("=== Generator Demonstration ===")
+    print(f"Fibonacci sequence (first {count}): ", end="")
+    for i in fibonacci_sequence(count):
+        print(i, end="")
+        if t > 1:
+            print(", ", end="")
+        t -= 1
+    print(end="\n")
+    get_next_prim = prime_numbers()
+    count = 5
+    print(f"Prime numbers (first {count}): ", end="")
+    for i in range(count):
+        print(next(get_next_prim), end="")
+        if i != count - 1:
+            print(", ", end="")
+    print(end="\n")
 
 
 def main():
@@ -44,12 +82,15 @@ def main():
     high_level = 0
     treasure = 0
     level_up = 0
+    print("=== Game Data Stream Processor ===\n")
+    print("Processing 1000 game events...\n")
     events = generate_game_events(num_events)
     for event in events:
         total_events += 1
         if buffer_displaying > 0:
-            print(f"Player {event["Player"]} (level {event["LvL"]})"
-                  f" {type_events[event["type"]]}")
+            print(f"Event {total_events}: Player {event['Player']}"
+                  f" (level {event['LvL']})"
+                  f" {type_events[event['type']]}")
             buffer_displaying -= 1
         if event["LvL"] >= 10:
             high_level += 1
@@ -57,10 +98,13 @@ def main():
             treasure += 1
         if event["type"] == 4:
             level_up += 1
-    print(end="\n")
+    print("...\n")
     show_analytics(total_events, high_level, treasure, level_up)
     print(end="\n")
     compute_time()
+    print(end="\n")
+    generator_demonstration()
+
 
 if __name__ == "__main__":
     main()
