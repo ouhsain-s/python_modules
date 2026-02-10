d608ac2aced8e317b5b7b5d02ca08e4216d7133b
Salahdine Ouhsain <souhsain@e2r6p14.1337.ma>
2026-02-10 18:03:31 +0100

    add

--- module04/ex1/ft_archive_creation.py
+++ module04/ex1/ft_archive_creation.py
@@ -2,7 +2,11 @@ def main() -> None:
     discovered = ["[ENTRY 001] New quantum algorithm discovered\n",
                   "[ENTRY 002] Efficiency increased by 347%\n",
                   "[ENTRY 003] Archived by Data Archivist trainee\n"]
-    file1 = open("new_discovery.txt", "w")
+    try:
+        file1 = open("new_discovery.txt", "w")
+    except Exception:
+        print("ERROR: Failed to preserve data.")
+        return
     print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
     print(f"Initializing new storage unit: {file1.name}")
     print("Storage unit created successfully...\n")
