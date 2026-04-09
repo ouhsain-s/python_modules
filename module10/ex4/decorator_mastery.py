3bc9c009b70a8e3271dfc0262228c712d3788678
Salahdine Ouhsain <souhsain@e2r3p14.1337.ma>
2026-04-09 14:59:10 +0100

    add

--- module10/ex4/decorator_mastery.py
+++ module10/ex4/decorator_mastery.py
@@ -1,8 +1,35 @@
-def spell_timer(func: Callable) -> Callable
-def power_validator(min_power: int) -> Callable
-def retry_spell(max_attempts: int) -> Callable
+import time
+from functools import wraps
+from collections.abc import Callable
 
-class MageGuild:
-    @staticmethod
-    def validate_mage_name(name: str) -> bool
-    def cast_spell(self, spell_name: str, power: int) -> str
+
+def spell_timer(func: Callable) -> Callable:
+    @wraps(func)
+    def wrapper():
+        print(f"Casting {func.__name__}...")
+        start = time.time()
+        func()
+        end = time.time()
+        print(f"Spell completed in {(end - start):.3f} seconds")
+    return wrapper
+
+# def power_validator(min_power: int) -> Callable:
+#     @wraps()
+# def retry_spell(max_attempts: int) -> Callable
+
+# class MageGuild:
+#     @staticmethod
+#     def validate_mage_name(name: str) -> bool
+#     def cast_spell(self, spell_name: str, power: int) -> str
+
+
+@spell_timer
+def fireball():
+    for _ in range(10000000):
+        pass
+
+
+if __name__ == "__main__":
+    print("Testing spell timer...")
+    fireball()
+    print(f"Result {fireball.__name__.capitalize()} cast!")
